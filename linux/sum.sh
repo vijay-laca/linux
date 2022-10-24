4f9b9a9aac28728212a52a20266016708aa83065
VIJAY VINOD <84370779+vijay-laca@users.noreply.github.com>
2022-10-24 02:04:06 -0400

    Add files via upload

---
+++ linux/sum.sh
@@ -0,0 +1,10 @@
+echo -n "Enter nth number's value:"
+read digit
+t=1
+total=0
+while test $t -le $digit
+do
+        total=`expr $total + $t`
+        t=`expr $t + 1`
+done
+echo "SUM OF $digit : $total "
